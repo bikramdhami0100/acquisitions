3a5ef91e0c6479b681dbbdb34123af049f98ac4d
bikramdhami0100 <bikramdhami334@gmail.com>
2025-09-19 06:35:57 +0545

    push

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app=express();
+const PORT=process.env.PORT || 3000;
+
+// app.listen(PORT,()=>{
+//     console.log(`server is running on port ${PORT}`);
+// });
+
+app.get("/",(req,res)=>{
+    res.status(200).send("Hello World");
+})
+
+export default app;
